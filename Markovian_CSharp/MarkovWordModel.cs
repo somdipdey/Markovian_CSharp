276494da0d3135f9ff568f1fe967f7fbfb1cea0f
Somdip Dey <somdip007@hotmail.com>
2018-01-20 15:42:53 +0000

    * Markovian_CSharp.csproj: added Markov Word Model for N words
    * MarkovModel.cs:
    * MarkovWordModel.cs:

--- Markovian_CSharp/MarkovWordModel.cs
+++ Markovian_CSharp/MarkovWordModel.cs
@@ -7,25 +7,25 @@ namespace Markovian_CSharp
     // Author: Somdip Dey, 2018
     // Description: This library provides a base class for Markov Model 
     //for random text generation and prediction.
-    public class MarkovModel
+    public class MarkovWordModel
     {
         private int keyLength;
-        private String computedText;
+        private String[] computedText;
         private Random myRandom;
 
         // <summary>
         // Constructor for Markov Model class
         // </summary>
-        public MarkovModel(int NumberOfCharacters)
+        public MarkovWordModel(int NumberOfWords)
         {
-            keyLength = NumberOfCharacters;
+            keyLength = NumberOfWords;
             myRandom = new Random();
         }
 
         // <summary>
-        // Get the computed training text for the Markov Model
+        // Get the computed training text split in words for the Markov Model
         // </summary>
-        public String GetTrainingText()
+        public String[] GetTrainingText()
         {
             return computedText;
         }
@@ -52,14 +52,14 @@ namespace Markovian_CSharp
         // </summary>
         public void SetTraining(String TrainingText)
         {
-            computedText = TrainingText.Trim();
+            computedText = TrainingText.Split(' ');
         }
 
         // <summary>
         // Predicts the next character by finding all the characters that follow a substring of keyLength
         // characters in the training text, and then randomly picking one of them as the next character.
         // </summary>
-        public String GetRandomText(int NumberOfCharacters)
+        public String GetRandomText(int NumberOfWords)
         {
             if (computedText == null)
             {
@@ -68,13 +68,22 @@ namespace Markovian_CSharp
             StringBuilder sb = new StringBuilder();
             // Generate a random index from valid indexes (those that have following characters)
             int index = myRandom.Next(computedText.Length - keyLength);
-            // Assign to key the character string at the random index that is keyLength long
-            String key = computedText.Substring(index, keyLength);
-            sb.Append(key);
-            // NOTE: Generate numChars minus four cuz they are set before the loop
-            for (int k = 0; k < NumberOfCharacters - keyLength; k++)
+            // Assign to key the word at the random index that is keyLength long
+            String[] key = new string[keyLength];
+            for (int i = 0; i < keyLength; i++)
             {
-                // Find all characters that follow the current 4-character string
+                if (index + i < computedText.Length)
+                {
+                    key[i] = computedText[index + i];
+                    sb.Append(key[i]);
+                    sb.Append(' ');
+                }
+            }
+
+            // NOTE: Generate numWords minus keyLength cuz they are set before the loop
+            for (int k = 0; k < NumberOfWords - keyLength; k++)
+            {
+                // Find all words that follow the current word(s) string
                 List<String> follows = GetFollows(key);
                 // Break if no characters were found
                 if (follows.Count == 0)
@@ -85,8 +94,17 @@ namespace Markovian_CSharp
                 index = myRandom.Next(follows.Count);
                 String successor = follows[index];
                 sb.Append(successor);
-                // Combine old key (except first character) with successor to make next key
-                key = key.Substring(key.Length - (keyLength - 1)) + successor;
+
+                // Combine old key (except first word) with successor to make next key
+                String[] temp = new String[key.Length];
+                for (int j = 0; j < key.Length; j++)
+                {
+                    if (j + 1 < key.Length)
+                        temp[j] = key[j + 1];
+                    else
+                        temp[j] = successor;
+                }
+                temp.CopyTo(key, 0);
             }
             return sb.ToString();
         }
@@ -97,29 +115,59 @@ namespace Markovian_CSharp
         // follow key and puts all these characters into an list of Strings.
         // @returns the List of String
         // </summary>
-        public List<String> GetFollows(String key)
+        public List<String> GetFollows(String[] key)
         {
             List<String> follows = new List<String>();
-            // Loop through computedText until no more characters are found
-            int pos = 0;
+            // Loop through computedText until no more words are found
+            int index = 0;
             while (true)
             {
                 // Find indexes of key and succeeding character
-                int index = computedText.IndexOf(key, pos);
-                int indexOfSuccessor = index + key.Length;
+                index = IndexOf(computedText, key, index);
+
                 // Break if key isn't found or if successor index is greater than last index
-                if (index == -1 || indexOfSuccessor >= computedText.Length)
+                if (index == -1 || (index + 1) >= computedText.Length)
                 {
                     break;
                 }
                 // Add to ArrayList the character immediately after key
-                String successor = computedText.Substring(indexOfSuccessor, 1);
+                String successor = computedText[index + 1];
                 follows.Add(successor);
                 // Update search position in computedText
-                pos = index + 1;
+                index += 1;
             }
             return follows;
         }
 
+        // Method to search index of target
+        /**
+         * Looks at the start position and returns the first index location in words that
+         * matches target. If no word is found, then returns -1.
+         */
+        private int IndexOf(String[] words, String[] target, int start)
+        {
+            int index = -1;
+
+            for (int i = start; i < words.Length - (target.Length - 1); i++)
+            {
+                int count = 0;
+                int matched = 0;
+                while (count < target.Length)
+                {
+                    if (words[i + count] == target[count])
+                    {
+                        matched++;
+                    }
+                    count++;
+                }
+
+                if (matched == target.Length)
+                {
+                    return i + target.Length;
+                }
+            }
+
+            return index;
+        }
     }
 }
